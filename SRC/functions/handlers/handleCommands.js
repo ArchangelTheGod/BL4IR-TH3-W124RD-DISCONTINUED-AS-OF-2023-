ed4e757a16434a9ab26a69a4827a6e30e628d5e6
Archangel <86256017+AxxlYT@users.noreply.github.com>
2023-06-20 15:01:58 -0400

    Add files via upload

---
+++ SRC/functions/handlers/handleCommands.js
@@ -0,0 +1,38 @@
+const { REST } = require("@discordjs/rest");
+const { Routes } = require("discord-api-types/v9");
+const chalk = require(`chalk`);
+const fs = require("fs");
+
+module.exports = (client) => {
+  client.handleCommands = async () => {
+    const commandFolders = fs.readdirSync("./src/commands");
+    for (const folder of commandFolders) {
+      const commandFiles = fs
+        .readdirSync(`./src/commands/${folder}`)
+        .filter((file) => file.endsWith(".js"));
+
+      const { commands, commandArray } = client;
+      for (const file of commandFiles) {
+        const command = require(`../../commands/${folder}/${file}`);
+        commands.set(command.data.name, command);
+        commandArray.push(command.data.toJSON());
+      }
+    }
+
+    const clientId = "1111728534059229355";
+    const guildId = ("1034651087187222571");
+
+    const rest = new REST({ version: "9" }).setToken(process.env.TOKEN2);
+    try {
+      console.log(chalk.blue(`Loading Commands...`));
+
+      await rest.put(Routes.applicationGuildCommands(clientId, guildId), {
+        body: client.commandArray,
+      });
+
+      console.log(chalk.green(`Commands Loaded.`));
+    } catch (error) {
+      console.error(error);
+    }
+  };
+};
