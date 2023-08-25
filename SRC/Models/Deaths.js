16e8ec6adfbcf5e96b9848c6e4be5e7ebd807795
Archangel <86256017+ArchangelTheGod@users.noreply.github.com>
2023-08-25 13:58:45 -0400

    Delete Deaths.js

--- SRC/Models/Deaths.js
+++
@@ -1,9 +0,0 @@
-const { model, Schema } = require('mongoose');
-
-    let deathSchema = new Schema({
-      User: String,
-      Deaths: Number,
-      UserName: String,
-});
-
-module.exports = model('deaths', deathSchema);
